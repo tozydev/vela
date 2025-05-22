1e0918b68480075b6ccfd27f884fe865e75f9a11
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-05-22 21:04:57 +0700

    feat: add utility to determine MIME type based on file extension

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+const mimeTypes: { [key: string]: string } = {
+  "jar": "application/java-archive",
+  "xml": "application/xml",
+  "json": "application/json",
+  "zip": "application/zip",
+  "module": "application/vnd.org.gradle.module+json",
+  "asc": "application/pgp-signature",
+  "md5": "text/plain",
+  "sha1": "text/plain",
+  "sha256": "text/plain",
+  "sha512": "text/plain"
+}
+
+export const getMimeType = (path: string) => {
+  const extension = path.split(".").pop()
+  return extension ? mimeTypes[extension] : null
+}

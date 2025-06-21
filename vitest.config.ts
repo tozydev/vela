d0a7f6aaf8bc3ba25be7f5af2e0a8ab09ca4bb07
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-06-21 23:52:00 +0700

    build: add coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config"
 export default defineWorkersConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: "istanbul"
+    },
     poolOptions: {
       workers: {
         singleWorker: true,

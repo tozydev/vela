a52b47cd3004b1a4c706be7d2b2caa4948e08059
Nguyễn Thanh Tân <me@tozydev.id.vn>
2025-06-21 23:33:27 +0700

    build: simplify build and deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin"
-import build from "@hono/vite-build/cloudflare-workers"
 import { defineConfig } from "vite"
-import ssrHotReload from "vite-plugin-ssr-hot-reload"
 
-export default defineConfig(({ command, isSsrBuild }) => {
-  if (command === "serve") {
-    return { plugins: [ssrHotReload(), cloudflare()] }
-  }
-  if (!isSsrBuild) {
-    return {
-      build: {
-        rollupOptions: {
-          input: ["./src/style.css"],
-          output: {
-            assetFileNames: "assets/[name].[ext]"
-          }
-        }
-      }
+export default defineConfig({
+    plugins: [cloudflare()],
+    build: {
+      minify: true
     }
   }
-  return {
-    // @ts-ignore
-    plugins: [build({
-      outputDir: "dist-server"
-    })]
-  }
-})
+)
